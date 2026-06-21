88efc1dbfe3a0ae5ff32482a1f4367d1dcad0ba5
roshan22418 <roshan22418@iiitd.ac.in>
2026-06-21 16:10:31 +0530

    add min and max by using random

--- JavaSript/numberAndMath.js
+++ JavaSript/numberAndMath.js
@@ -29,7 +29,7 @@ const numberHundred = 100000000000
 // console.log(Math.floor(3.6))
 
 
-console.log(Math.floor(Math.random()*10)+1)
+console.log(Math.random()*10)
 
 
 // important point 
@@ -38,6 +38,3 @@ const min = 10
 const max = 20
 
 console.log(Math.floor(Math.random()*(max-min+1))+min)
-
-
-
