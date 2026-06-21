52663bb2afd991ba7e9c9dc0fb8e0a4d369e04f2
roshan22418 <roshan22418@iiitd.ac.in>
2026-06-21 15:57:20 +0530

    add number and Math function

---
+++ JavaSript/numberAndMath.js
@@ -0,0 +1,43 @@
+const num = 23213
+
+
+const num1 = new Number(34343)
+// console.log(num1.toString().length)
+// console.log(num1.toFixed(2))
+
+const newNumber = 2343.393
+
+// console.log(newNumber.toPrecision(5))
+
+
+const numberHundred = 100000000000
+
+// console.log(numberHundred.toLocaleString('en-IN'))
+
+
+
+
+
+
+//++++++++++++++++++++++++++ Maths ++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-3434))
+// console.log(Math.round(3.23))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(3.6))
+
+
+console.log(Math.floor(Math.random()*10)+1)
+
+
+// important point 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
